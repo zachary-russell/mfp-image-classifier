2f0e75554bfc702b119750634ddfdab4fb5ff55a
Zach Russell <zach@protechig.com>
2021-03-31 13:36:10 -0400

    feat: basic app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+import 'dart:io';
+import 'package:flutter/services.dart' show rootBundle;
+
 import 'package:flutter/material.dart';
+import 'package:csv/csv.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:share/share.dart';
 
 void main() {
   runApp(MyApp());
@@ -22,92 +28,179 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(
+        title: 'MFP Photo Classifier',
+        key: ValueKey('app'),
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, this.title}) : super(key: key);
+  MyHomePage({required Key key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String? title;
+  final String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<List<dynamic>> imageData = [];
+  int currentIndex = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
+      _parseCsv();
     });
+  }
+
+  Future<void> _writeToCsv() async {
+    String csv = const ListToCsvConverter().convert(imageData);
+    final path = await _localPath;
+    File file = File('$path/images.csv');
+    await file.writeAsString(csv);
+    print('saved local file');
+  }
+
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    // ignore: unrelated_type_equality_checks
+    if (File('$path/images.csv').existsSync()) {
+      print('loaded local file');
+      return File('$path/images.csv');
+    } else {
+      throw ('foo');
+    }
+  }
+
+  Future<void> _parseCsv() async {
+    var file;
+    try {
+      final localFile = await _localFile;
+      file = await localFile.readAsString();
+    } catch (e) {
+      print(e);
+      print("loading base file");
+      file = await rootBundle.loadString('assets/images.csv');
+    }
+
+    List<List<dynamic>> foo = CsvToListConverter().convert(file);
+    foo.forEach((element) {
+      element.length = 4;
+    });
+    setState(() {
+      imageData = foo;
+    });
+    print('parsing CSV');
+  }
+
+  Future<void> _classifyImage(bool isPhoto) async {
+    setState(() {
+      imageData[currentIndex][3] = isPhoto;
+    });
+    setState(() {
+      currentIndex = currentIndex + 1;
+    });
+    print(currentIndex);
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title!),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.save,
+                color: Colors.white,
+              ),
+              onPressed: () async {
+                await _writeToCsv();
+                // do something
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            IconButton(
+              icon: Icon(
+                Icons.share,
+                color: Colors.white,
+              ),
+              onPressed: () async {
+                final path = await _localPath;
+                Share.shareFiles(['$path/images.csv']);
+                // do something
+              },
+            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Center(
+            child: (imageData.length < 1)
+                ? CircularProgressIndicator()
+                : Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Image.network(
+                        imageData[currentIndex][1],
+                        errorBuilder:
+                            (BuildContext context, Object exception, _) {
+                          return Text(
+                              'issue loading image, please go to the next image');
+                        },
+                      ),
+                      Text('Is this image a photograph?'),
+                      (imageData[currentIndex][3] != null)
+                          ? Text(
+                              'Current Choice ${imageData[currentIndex][3].toString()}')
+                          : SizedBox(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          IconButton(
+                              icon: Icon(Icons.skip_previous),
+                              onPressed: () {
+                                if (currentIndex != 0) {
+                                  setState(() {
+                                    currentIndex--;
+                                  });
+                                }
+                                ;
+                              }),
+                          ElevatedButton.icon(
+                              onPressed: () async {
+                                await _classifyImage(true);
+                              },
+                              icon: Icon(Icons.check),
+                              label: Text('Yes')),
+                          ElevatedButton.icon(
+                              onPressed: () async {
+                                await _classifyImage(false);
+                              },
+                              icon: Icon(Icons.close),
+                              label: Text('No')),
+                          IconButton(
+                              icon: Icon(Icons.skip_next),
+                              onPressed: () {
+                                if (currentIndex != imageData.length - 1) {
+                                  setState(() {
+                                    currentIndex++;
+                                  });
+                                }
+                                ;
+                              }),
+                        ],
+                      )
+                    ],
+                  )));
   }
 }
